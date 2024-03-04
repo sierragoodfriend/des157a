27a0cee545db8c99db44f0a7c878e84f343cd171
Sierra Goodfriend <sierragoodfriend@gmail.com>
2024-03-04 11:32:17 -0800

    make winning pop up and change some styles in css + JS for stu3

--- studio3/script.js
+++ studio3/script.js
@@ -10,14 +10,15 @@
     const player1 = document.querySelector(`#leftColumn`); 
     const player2 = document.querySelector(`#rightColumn`);
     const scorePerRound = document.querySelector(`#middleColumn`);
+    const middleColumnContainer = document.querySelector(`#flexWrapColumns`);
     const actionArea = document.querySelector(`#action`);
     const quitBtn = document.querySelector(`#quit`);
-    // const winner = document.querySelector(`#winnerPopUp`);
+    const winnerPopUp = document.querySelector(`#youWon`);
 
     const displayDice = document.querySelector(`#dice`);
 
     const gameData = {
-        dice: ['images/1die.jpg', 'images/2die.jpg', 'images/3die.jpg', 'images/4die.jpg', 'images/5die.jpg', 'images/6die.jpg'],
+        dice: ['images/1die.png', 'images/2die.png', 'images/3die.png', 'images/4die.png', 'images/5die.png', 'images/6die.png'],
         players: ['Player 1', 'Player 2'],
         score: [0, 0],
         roll1: 0,
@@ -32,7 +33,6 @@
     startGame.addEventListener(`click`, function(){
         //new styles for header
         h1Playing.style.width = `100%`;
-        h1Playing.style.height = `100px`;
         h1Playing.style.backgroundColor = `rgb(234, 199, 235)`;
 
         //remove footer styles
@@ -51,10 +51,12 @@
         scorePerRound.style.color = `rgb(79, 29, 67)`;
         scorePerRound.style.paddingTop = `40px`;
         scorePerRound.style.marginTop = `35px`;
-        
+        scorePerRound.style.fontFamily = `"Anta", sans-serif`;
+
         //Update player 1 and 2 scores for HTML
         updatePlayerScore();
 
+        //put pigs on the page
         player1.innerHTML = `<img src="images/pigImgLeft.png" id="inGamePigImg0" alt="Cute Pig 1" width="230.7" height="206.9">`; //replace left side
         player2.innerHTML = `<img src="images/pigImgRight.png" id="inGamePigImg1" alt="Cute Pig 2" width="230.7" height="206.9">`; //replace right side
         //middle column
@@ -71,7 +73,11 @@
 
     //Announces who's turn it is, and adds roll dice button – throwDice(); gets run when Roll Dice! is pressed
     function setUpTurn(){
-
+            //stop displaying dice after a players turn
+            displayDice.innerHTML = ``;
+            //redo styles for when a player switches turns
+            h1Playing.style.marginBottom = `83.3px`;
+            middleColumnContainer.style.marginBottom = `50px`;
             //add & delete class animation on pigs depending on whose turn it is
             const currentPlayer = document.querySelector(`#inGamePigImg${gameData.index}`);
             currentPlayer.classList.add(`playingPigAnimation`);
@@ -90,9 +96,15 @@
 
     //runs through all possible dice rolls and their consequences / affect on scoring
     function throwDice(){
+        //header and flexcontainer (for roundscore area) has less margin bottom so lower buttons can fit
+        h1Playing.style.marginBottom = `40.3px`;
+        middleColumnContainer.style.marginBottom = `0px`;
+
+        //math for rolls of die when they're thrown
         actionArea.innerHTML = ``;
         gameData.roll1 = Math.floor(Math.random() * 6 + 1); //Math.floor() rounds down
         gameData.roll2 = Math.floor(Math.random() * 6 + 1); //gives nums between 1-6
+
         displayDice.innerHTML = `<img src="${gameData.dice[gameData.roll1 - 1]}"><img src="${gameData.dice[gameData.roll2 - 1]}">`; //show dice rolled on page
         gameData.rollSum = gameData.roll1 + gameData.roll2; //total of that roll
 
@@ -108,7 +120,7 @@
             //Update player 1 and 2 scores for HTML
             updatePlayerScore();
             
-            setTimeout(setUpTurn, 2000);
+            setTimeout(setUpTurn, 1400);
 
         } else if(gameData.roll1 === 1 || gameData.roll2 === 1){
 
@@ -121,7 +133,7 @@
 
             gameData.index ? (gameData.index = 0) : (gameData.index = 1); //switch players
 
-            setTimeout(setUpTurn, 2000);
+            setTimeout(setUpTurn, 1400);
 
         } else {
 
@@ -133,17 +145,19 @@
 
             updatePlayerScore();
 ;
-            actionArea.innerHTML = `<button id="rollagain" class="roll">Roll Again?</button><button id="pass" class="roll">Pass</button>`;
+            actionArea.innerHTML = `<button id="rollagain" class="roll">Roll Again!</button><button id="pass" class="roll">Pass</button>`;
 
             //roll again button
             document.querySelector(`#rollagain`).addEventListener(`click`, function(){
 
-                setUpTurn();
+            throwDice();
 
             });
 
             //Pass button – passing a turn adds your roundSum score to your total score up at the top
             document.querySelector(`#pass`).addEventListener(`click`, function(){
+                h1Playing.innerHTML = `switching players...`;
+
                 gameData.score[gameData.index] += gameData.roundSum[gameData.index]; //score equal to the round sum when passing
                 gameData.roundSum[gameData.index] = 0;
                 const currentPlayerScore = gameData.index === 0 ? player1Score : player2Score; //if it's player 1's turn, currentPlayerScore = player1Score, if not, then currentPlayerScore = player2Score
@@ -152,7 +166,8 @@
                 [gameData.index]; //update score array & HTML on page
                 gameData.index ? (gameData.index = 0) : (gameData.index = 1); //switch players
                 scorePerRound.innerHTML = 0;
-                setUpTurn();
+
+                setTimeout(setUpTurn, 1400);
             });
 
             console.log(gameData.score);
@@ -160,19 +175,25 @@
     }
 
     function updatePlayerScore() {
+
+        //check winning condition here
+        //have pop-up display flex instead of hidden
+        if(gameData.score[gameData.index] > gameData.gameEnd) {
+            winnerPopUp.innerHTML = `Congrats ${gameData.players[gameData.index]}, you won with ${gameData.score[gameData.index]} points! <button id="goHome">Go home!</button>`;
+            winnerPopUp.style.display = `flex`;
+
+            //declare go home btn after it's added to html
+            const goHomeBtn = document.querySelector(`#goHome`);
+
+            goHomeBtn.addEventListener(`click`, function(){
+                location.reload();
+            });
+        } else {
         player1Score.innerHTML = `Player 1<br>${gameData.score[0]}`; 
         player2Score.innerHTML = `Player 2<br>${gameData.score[1]}`;
+        }
     }
-
-    //still to create fix winning condition
+    
 
 })();
 
-
-
-// reload 
-// gameControl.innerHTML += `<button id="quit">Wanna Quit?</button>`;
-
-//         document.getElementById(`quit`).addEventListener(`click`, function(){
-//             location.reload();
-//         });
